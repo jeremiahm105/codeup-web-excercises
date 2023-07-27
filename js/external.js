9defe7245a922ec0d9b2be00550fdcd4bfde9111
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-07-27 14:50:31 -0500

    Questions 1 and 2 complete

--- js/external.js
+++ js/external.js
@@ -1,6 +1,6 @@
-"use strict";
 
 
+/*
 console.log("Hey! Im external JS")
 
 console.log("Heres another test")
@@ -10,5 +10,18 @@ let cohort = "Fenir"
 alert("Hey there, " + cohort);
 prompt("Did you have a good lunch");
 confirm("Are you sure")
-confirm("Are you sure")
+*/
+
+console.log("Complete Exercise")
+
+
+alert("Welcome to my Website")
+let favcolor = prompt("What is you favorite color")
+let coloranswer = confirm("Great " + favcolor + " is my favorite color as well!! ")
+
+//Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
+//
+// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+// Use an alert to show the results of each problem.
+// Finally, commit the changes to your git repository, and push to GitHub.
 
