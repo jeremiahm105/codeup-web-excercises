f0b482606cb9368025b2dfc1e7e4d7857af7d583
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-07-27 14:26:31 -0500

    First commit complete

---
+++ js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+console.log("Hey! Im external JS")
+
+console.log("Heres another test")
+
+let cohort = "Fenir"
+
+alert("Hey there, " + cohort);
+prompt("Did you have a good lunch");
+confirm("Are you sure")
+
