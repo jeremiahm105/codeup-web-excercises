674cc18dc73a5fdb5e0d344103bbbf25fc2d7d58
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-07-28 01:37:26 -0500

    Modified Question 3.1

--- js/external.js
+++ js/external.js
@@ -1,27 +1,14 @@
+let LM = prompt("How many days would you like to rent the Little Mermaid?")
+let BB = prompt("How many days would you like to rent the Brother Bear?")
+let HERC = prompt("How many days would you like to rent the Hercules?")
+let PPD = 3
+
+let LMtotal  = (LM * PPD)
+let BBtotal  = (BB * PPD)
+let HERCtotal = (HERC * PPD)
+
+let OverAllTotal= LMtotal + BBtotal + HERCtotal
 
 
-/*
-console.log("Hey! Im external JS")
-
-console.log("Heres another test")
-
-let cohort = "Fenir"
-
-alert("Hey there, " + cohort);
-prompt("Did you have a good lunch");
-confirm("Are you sure")
-*/
-
-console.log("Complete Exercise")
-
-
-alert("Welcome to my Website")
-let favcolor = prompt("What is you favorite color")
-let coloranswer = confirm("Great " + favcolor + " is my favorite color as well!! ")
-
-//Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
-//
-// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-// Use an alert to show the results of each problem.
-// Finally, commit the changes to your git repository, and push to GitHub.
+alert("Total cost for all movies rented will be: $" + OverAllTotal.toFixed(2))
 
