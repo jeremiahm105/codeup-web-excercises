28311e556eea8804f8d4662e5cf6da9dbe882434
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-09-06 14:43:11 -0500

    JQuery Selectors Ex.

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,55 @@
 "use strict";
 
-$(function() {
+$(function () {
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
-    alert( 'The DOM has finished loading!' );
+    //alert( 'The DOM has finished loading!' );
 
+
+    // console.log($("p"));
+    // console.log($('li').html());
+    // //console.log($('p').css("border"));
+    //
+    // // grab elements and store results into variables
+    //
+    // let borderprop = $('p').css("border");
+    //
+    // $("li").css('border', '1px solid red')
+    // $("*").css("color", "purple")
+
+    console.log($("p"));
+    console.log($("#ice-cream-list"));
+
+    //     Class Selectors
+    //
+    // Remove your custom jQuery code from previous exercises.
+    //
+    //     Update your code so that at least 3 different elements have the same class named codeup.
+    //
+    //     Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+    $(".codeup").css("border", "1px dashed red")
+    //
+    //     Remove the class from one of the elements. Refresh and test that the border has been removed.
+    //
+    //     Give another element an id of codeup. Does this element get a border now?
+    //
+    //     Element Selectors
+    //
+    // Remove your custom jQuery code from previous exercises.
+    //
+    //     Using jQuery, set the font-size of all li elements to 20px.
+    $("li").css("font-size", "20px")
+    //
+    //     Craft selectors that highlight all the h1, p, and li elements.
+    $("h1, p, li").css('background-color', "yellow")
+    //     Create a jQuery statement to alert the contents of your h1 element(s).
+    //
+    //     Multiple Selectors
+    //
+    // Combine your selectors that highlight all the h1, p, and li elements.
+    //$('h1, p, li').css("border", "3px solid red")
 
 });
-
 
 
 
--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,7 +3,7 @@
 $(function() {
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
-    alert( 'The DOM has finished loading!' );
+    //alert( 'The DOM has finished loading!' );
 
 
 });
@@ -47,14 +47,55 @@ $(function() {
 //
 // We will add some jQuery to select all the elements with a class of important and use the .css method to change the background color to yellow.
 
-$('.important').css('background-color', 'yellow')
 
-ELEMENT SELECTOR EXAMPLES
-$('tag_name')
-$('p')
+// lecture notes
 
-Using the Multiple Selector SEPERATE BY COMMA
-$('.important, p').css('background-color', '#FF0');
-
-The All Selector USES THE WILDCARD ASTERISK *
-$('*')
+// console.log($("p"));
+//
+// console.log($("#ice-cream-list"));
+//
+// console.log($(".list-item"));
+//
+// //       Okay - so we can use CSS selector syntax and get back collections of matching. . ids, classes, elements, etc. What now?
+//
+// //.html()
+// //jQuery - console.log the innerHtml of a p tag
+// console.log($("p").html());
+//
+// // Retrieves first of available (similar to querySelector)
+// console.log($("li").html());
+//
+// //.css() go GET a property from selector
+//
+// console.log($("p").css("border"));
+//
+// //    Let's see- can I get the return values and store them in vars?
+//
+// let borderProperties = $("p").css("border");
+//
+// console.log(`Hey : The border properties are ${borderProperties}`);
+//
+// //       So we used html() and css() to go GET information - let's try setting some information
+//
+// let interestingParagraph = "I went down the street the other day to go get some ice cream, and you wouldn't believe what happened to me on the way. I ran into a cop car - the officer got out of the vehicle and said 'HEY' 'WHAT ARE YOU DOING' and I said 'I am on the way to go get some ice cream'. Then I got a ticket, and I went on to have some ice cream: I had an almond joy type of thing. . kinda chocolatey, IDK, you know what I mean";
+//
+// $("p").html(interestingParagraph);
+//
+// //       Let's change the color of the paragraph to something less aggressive - like a nice cornflowerblue <3<3<3
+//
+// $("p").css("color", "cornflowerblue");
+//
+// //       We now see .html() and .css() can SET information with argument(s)
+//
+// // Method chaining : .method().method().method().method().method()
+// $("li")
+//     .css("border", "1px dashed red")
+//     .css("margin", ".2em")
+//     .css("padding", "2em");
+//
+// //       Multiple selectors and all selectors
+//
+// console.log($("#ice-cream-list, h1")
+//     .html());
+//
+// $("*").css("color", "rebeccapurple");
